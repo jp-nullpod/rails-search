0e001d5f31beb2963f8a0f175cbbe66e15c521ce
Jp Fortuno <jp.fortuno@nullpod.com>
2023-05-31 22:34:10 +0400

    multiple search techniques

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,25 @@ class MoviesController < ApplicationController
     def index
         @movies = Movie.all
         if params[:query].present?
-            sql_subquery = "title ILIKE :query OR synopsis ILIKE :query"
-            @movies = @movies.where(sql_subquery, query: "%#{params[:query]}%")
+            # sql_subquery = "title ILIKE :query OR synopsis ILIKE :query"
+            # @movies = @movies.where(sql_subquery, query: "%#{params[:query]}%")
+            
+            # sql_subquery = <<~SQL
+            # movies.title ILIKE :query
+            # OR movies.synopsis ILIKE :query
+            # OR directors.first_name ILIKE :query
+            # OR directors.last_name ILIKE :query
+            # SQL
+            # @movies = @movies.joins(:director).where(sql_subquery, query: "%#{params[:query]}%")
+
+            # sql_subquery = <<~SQL
+            # movies.title @@ :query
+            # OR movies.synopsis @@ :query
+            # OR directors.first_name @@ :query
+            # OR directors.last_name @@ :query
+            # SQL
+            # @movies = @movies.joins(:director).where(sql_subquery, query: params[:query])
+
         end
     end
 end
