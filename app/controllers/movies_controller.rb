637076c656573698a0256f15ac20a118e3aee69e
Jp Fortuno <jp.fortuno@nullpod.com>
2023-05-31 22:26:10 +0400

    improving search and view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,9 @@
 class MoviesController < ApplicationController
     def index
         @movies = Movie.all
+        if params[:query].present?
+            sql_subquery = "title ILIKE :query OR synopsis ILIKE :query"
+            @movies = @movies.where(sql_subquery, query: "%#{params[:query]}%")
+        end
     end
 end
