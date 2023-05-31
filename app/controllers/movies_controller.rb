a8fda5c1b356da94509f06ca3f82c674db63b997
Jp Fortuno <jp.fortuno@nullpod.com>
2023-05-31 23:00:55 +0400

    tons of search awesomeness

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,7 +20,8 @@ class MoviesController < ApplicationController
             # OR directors.last_name @@ :query
             # SQL
             # @movies = @movies.joins(:director).where(sql_subquery, query: params[:query])
-
+            
+            # @movies = Movie.global_search(params[:query])
         end
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :director
 
   include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
+  
   pg_search_scope :search_by_title_and_synopsis,
   against: [ :title, :synopsis ],
   using: {
